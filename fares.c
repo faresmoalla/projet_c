27c1e9c9be4eca0bc08a666b8898a121a5c5d3f9
faresmoalla <fares.moalla@esprit.tn>
2020-11-02 11:53:16 +0100

    dernier modif

--- fares.c
+++ fares.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
+##include <stdio.h>
 #include "fonctions.h"
-int main () {
-int a ;
-int b ;
-printf("\nsaisie du premier nombre:");
-scanf("%d",a);
-do{
-printf("\nsaisie du second nombre:");
-scanf("%d",b);
-}
+int main() {
+int a;
+int b;
+printf("\nSaisie du Premier Nombre: ");
+scanf("%d", &a);
+do {
+printf("\nsaisie du Second Nombre: ");
+scanf("%d", &b);
 } while(b == 0);
 printf("\n%d + %d= %d", a, b, sommer(a, b));
 printf("\n%d - %d= %d", a, b, soustraire(a, b));
 printf("\n%d / %d = %f\n", a, b, diviser(a, b));
 return (0);
 }
-//programme
